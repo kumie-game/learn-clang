26845964749f69eb2e50d0bd5e49d65413de2ffe
t.ogawa <tomozo.oga.oga@gmail.com>
2019-05-16 22:54:30 +0900

    変数の有効範囲まで

--- main.c
+++ main.c
@@ -1,42 +1,24 @@
 #include <stdio.h>
 
 /*
-test comment
-*/
-/* 
- 関数 : 複数の処理
 
- 返り値の型　関数名(引数, ...){
-   処理;
-   return 返り値;
- }
-*/
-float getMax(float a, float b);
-/*プロトタイプ宣言*/
+変数の有効範囲
 
-void sayHi(void)
-{
-  printf("hi!\n");
+*/
+int a = 0; /*グローバル変数 */
+
+void f(void){
+ /* int a = 0; (ローカル変数)*/
+  a++;
+  printf("a:%d\n" , a);
+
 }
-int main(void)
-{
 
-  float result;
-  result = getMax(2.3, 5.2);
-  printf("%f\n", result);
+int main(void){
 
-  sayHi();
+  f();
+
+  printf("main:%d\n" , a);
 
   return 0;
-}
-float getMax(float a, float b)
-{
-  if (a >= b)
-  {
-    return a;
-  }
-  else
-  {
-    return b;
-  }
 }
