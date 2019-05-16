07f2e23ddab50c408de0f22ae3ba2761d5bc6195
t.ogawa <tomozo.oga.oga@gmail.com>
2019-05-16 23:33:16 +0900

    文字列

--- main.c
+++ main.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 
 /*
-配列
+文字列 : charの配列(※ 終端 「\0」)
+
+int i;
+
+× string s;
+
+char s[] = {'a' , 'b' , 'c' , '\0'};
+char s[] = "abc";
+char s[4] = "abc";
+
 */
+
 int main(void)
 {
+  char s[] = "abc";
 
-  /* int sales[3];
-
-  sales[0] = 200;
-  sales[1] = 400;
-  sales[2] = 300;
-  sales[3] = 300; */
-
-  int sales[3] = {200, 400, 300};
-
-  printf("%d\n", sales[2]);
+  printf("%c\n", s[1]);
 
   return 0;
 }
